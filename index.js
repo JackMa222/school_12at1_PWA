98d936cdccccaf3517e5d58a27ba81f0d2c184c2
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-11-18 12:48:54 +1100

    Introduce DB to index.js + plan offline

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
+// Introduce sqlite3 and database
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(".database/main_data.db")
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
