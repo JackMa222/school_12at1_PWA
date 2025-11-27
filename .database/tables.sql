89f8f66b629312d7b7bf80e36a3df7a8e1c80321
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-11-27 12:38:32 +1100

    Add book_collections table & modify verses & books

--- .database/tables.sql
+++ .database/tables.sql
@@ -4,13 +4,22 @@ CREATE TABLE Verses (
     chapter_number INTEGER,
     verse_number INTEGER,
     verse_text INTEGER,
-    FOREIGN KEY (book_id) REFERENCES Books(id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
 
 CREATE TABLE Books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     book_name TEXT NOT NULL,
     description TEXT,
-    book_id INTEGER NOT NULL,
-    short_name TEXT
+    book_id INTEGER NOT NULL UNIQUE,
+    book_collection_order_index INTEGER NOT NULL,
+    short_name TEXT,
+    FOREIGN KEY (book_collection_order_index) REFERENCES Books_collections(order_index)
+)
+
+CREATE TABLE Books_collections (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    description TEXT,
+    order_index INTEGER UNIQUE NOT NULL
 )
