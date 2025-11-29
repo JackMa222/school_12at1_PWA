ab1ab54d25ef4fd3ab91923e040d573db4d0e4ce
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-11-29 20:17:51 +1100

    Add helper function to export verses data to json

---
+++ util/verses.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const fs = require("fs");
+
+function exportVersesToJSON(db) {
+    return new Promise((resolve, reject) => {
+        const sql = `
+        SELECT
+            v.id AS verse_id,
+            v.book_id AS verse_book_id,
+            v.chapter_number,
+            v.verse_number,
+            v.verse_text,
+
+            b.id AS book_row_id,
+            b.book_name,
+            b.description AS book_description,
+            b.book_id AS book_number,
+            b.short_name,
+            b.book_collection_order_index,
+            bc.id AS collection_row_id,
+            bc.name AS collection_name,
+            bc.description AS collection_description,
+            bc.order_index AS collection_order_index
+
+        FROM Verses v
+        JOIN Books b
+        ON v.book_id = b.book_id
+        JOIN Books_collections bc
+        ON b.book_collection_order_index = bc.order_index
+        ORDER BY
+            bc.order_index,
+            b.book_id,
+            v.chapter_number,
+            v.verse_number
+        `;
+
+    db.all(sql, [], (error, rows) => {
+        if (error) return reject(error);
+
+        const outputDirectory = path.join(__dirname, "..", "public", "data");
+        const outputFile = path.join(outputDirectory, "verses.json");
+
+        if (!fs.existsSync(outputDirectory)) {
+            fs.mkdirSync(outputDirectory, { recursive: true });
+        }
+
+        fs.writeFile(outputFile, JSON.stringify(rows, null, 2), (error) => {
+            if (error) return reject (error);
+
+            console.log(`Exported ${rows.length} verses → ${outputFile}`);
+            resolve(true);
+        })
+    })
+    })
+}
+
+module.exports = exportVersesToJSON;
