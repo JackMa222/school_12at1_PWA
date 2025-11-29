3c9ae7f03048174ab599617c272c10ca1c5f1ab3
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-11-30 10:36:58 +1100

    Add verses.html code to books.html

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/verses", function (req, res) {
 });
 
 app.get("/books", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/html/verses.html"));
+  res.sendFile(path.join(__dirname, "public/html/books.html"));
 });
 
 app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ or http://127.0.0.1:8000 to access your website") );
