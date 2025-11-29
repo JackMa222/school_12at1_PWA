67ba8a57f1aa9a424bb38177f18d9d5da0a44aaf
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-11-30 10:36:15 +1100

    Duplicate verses.html file and code for books page

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ exportVersesToJSON(db).catch(error => console.error(error));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/html/index.html"));
 });
 
 app.get("/verses", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/verses.html"));
+  res.sendFile(path.join(__dirname, "public/html/verses.html"));
 });
+
+app.get("/books", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/html/verses.html"));
+});
+
 app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ or http://127.0.0.1:8000 to access your website") );
